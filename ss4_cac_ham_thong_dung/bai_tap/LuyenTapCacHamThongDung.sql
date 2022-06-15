9324ceb8eeea1931623773219512cfce6b06df68
Your Name <you@example.com>
2022-06-15 16:09:53 +0700

    commit

--- ss4_cac_ham_thong_dung/bai_tap/LuyenTapCacHamThongDung.sql
+++ ss4_cac_ham_thong_dung/bai_tap/LuyenTapCacHamThongDung.sql
@@ -1,16 +1,16 @@
-USE QuanLySinhVien;
+USE quan_ly_sinh_vien;
 -- Hiển thị tất cả các thông tin môn học (bảng subject) có credit lớn nhất.
 select * 
 from subject
 where credit >= all(select credit from subject);
 
 -- Hiển thị các thông tin môn học có điểm thi lớn nhất.
-select mark.MarkId, mark.mark, subject.SubName, subject.credit
-from mark join subject on mark.SubId = subject.SubId
+select mark.mark_id, mark.mark, subject.subject_name, subject.credit
+from mark join subject on mark.subject_id = subject.subject_id
 having mark.mark >= all(select max(mark) from mark);
 
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
 select *, avg(mark.mark) as 'diem_trung_binh'
-from student join mark on student.StudentId = mark.StudentId
-group by student.StudentId
+from student join mark on student.student_id = mark.student_id
+group by student.student_id
 order by avg(mark.mark) desc;
